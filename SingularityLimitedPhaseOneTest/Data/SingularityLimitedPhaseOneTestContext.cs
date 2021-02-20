cd79ab42a45ab72bfd3f0dd55a761bb9bf3d6ef4
zakariabijoy <zakaria@vonome.com>
2021-02-21 01:30:22 +0600

    Sedding data using entity framework fluent configuration

--- SingularityLimitedPhaseOneTest/Data/SingularityLimitedPhaseOneTestContext.cs
+++ SingularityLimitedPhaseOneTest/Data/SingularityLimitedPhaseOneTestContext.cs
@@ -16,6 +16,15 @@ namespace SingularityLimitedPhaseOneTest.Data
 
         public DbSet<Product> Product { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Product>()
+            .HasData(
+                new Product { Id = 1, Name = "Product 101" ,Description= "Product 101 des", SKU="#001", Price=10},
+                new Product { Id = 2, Name = "Product 103", Description = "Product 102 des", SKU = "#002", Price = 20 },
+                new Product { Id = 3, Name = "Product 102", Description = "Product 103 des", SKU = "#003", Price = 30 }
+            );
+        }
 
     }
 }
---
+++ SingularityLimitedPhaseOneTest/Migrations/20210220192819_seed product values.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SingularityLimitedPhaseOneTest.Migrations
+{
+    public partial class seedproductvalues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "Description", "Name", "Price", "SKU" },
+                values: new object[] { 1, "Product 101 des", "Product 101", 10.0, "#001" });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "Description", "Name", "Price", "SKU" },
+                values: new object[] { 2, "Product 102 des", "Product 103", 20.0, "#002" });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "Description", "Name", "Price", "SKU" },
+                values: new object[] { 3, "Product 103 des", "Product 102", 30.0, "#003" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
