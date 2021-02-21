b3e9917c861dc811417b11b092339d9103685bda
zakariabijoy <zakaria@vonome.com>
2021-02-21 18:27:35 +0600

    adding role to user

--- SingularityLimitedPhaseOneTest/Controllers/UsersController.cs
+++ SingularityLimitedPhaseOneTest/Controllers/UsersController.cs
@@ -49,7 +49,9 @@ namespace SingularityLimitedPhaseOneTest.Controllers
             {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
-                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                   
                 }),
                 Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
@@ -78,12 +80,14 @@ namespace SingularityLimitedPhaseOneTest.Controllers
             var u = new User
             {
                 UserName = model.UserName,
-                Password = model.Password
+                Password = model.Password,
+                Role = model.Role
             };
             _context.Users.Add(u);
             _context.SaveChanges();
 
             u.Password = "";
+           
 
             return Ok(u);
         }
