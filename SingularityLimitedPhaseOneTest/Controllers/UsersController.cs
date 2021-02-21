f4f5107ab5156acec63d5272b5dc7f2802f6637d
zakariabijoy <zakaria@vonome.com>
2021-02-21 17:12:33 +0600

    Add user controller with Authenticate Method and create jwt token on successful authentication

---
+++ SingularityLimitedPhaseOneTest/Controllers/UsersController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using SingularityLimitedPhaseOneTest.Data;
+using SingularityLimitedPhaseOneTest.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingularityLimitedPhaseOneTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly SingularityLimitedPhaseOneTestContext _context;
+        private readonly AppSettings _appSettings;
+
+        public UsersController(SingularityLimitedPhaseOneTestContext context, IOptions<AppSettings> appSettings)
+        {
+            _context = context;
+            _appSettings = appSettings.Value;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] User model)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.UserName == model.UserName && u.Password == model.Password);
+
+            //user is not found
+
+            if (user == null)
+            {
+                return BadRequest(new { message = "Username or Password is incorrect" });
+            }
+
+            //if user is found generate jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            return Ok(user);
+        }
+
+
+
+       
+    }
+}
